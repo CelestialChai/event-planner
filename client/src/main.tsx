e50a79f23daa9d5e40a28a83289e99c13ed436fb
OKenne <175995429+O-KenneDevWorks@users.noreply.github.com>
2024-11-19 20:43:25 -0500

    Added basic home page
    Updated Main.tsx and app.tsx to use router.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import HomePage from './pages/home.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      }, 
+      // {
+      //   path: '/show-volunteers',
+      //   element: <VolunteerPage />
+      // },
+      // {
+      //   path: '/new-volunteer',
+      //   element: <VolunteerForm />
+      // },
+      // {
+      //   path: '/edit-volunteer',
+      //   element: <EditVolunteer />
+      // },
+      // {
+      //   path: '/edit-work',
+      //   element: <EditWork />
+      // }
+    ]
+  }
+]);
+
+const rootElement = document.getElementById('root');
+if(rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <RouterProvider router={router} />
+  );
+}
