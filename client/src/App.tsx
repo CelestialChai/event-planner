2bc5da6cb6b8cb3ee527d5ca9843555ba063e8f3
OKenne <175995429+O-KenneDevWorks@users.noreply.github.com>
2024-11-26 20:51:47 -0500

    Disabled unused routes for deployment testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,10 @@ import "./App.css";
 import { Box, CssBaseline } from "@mui/material";
 import CreateEvent from "./pages/CreateEvent";
 import MyEvents from "./pages/myevents";
-import Unlocks from "./pages/unlocks";
-import AboutUs from "./pages/aboutus";
-import Venues from "./pages/venues";
-import Login from "./pages/login";
+// import Unlocks from "./pages/unlocks";
+// import AboutUs from "./pages/aboutus";
+// import Venues from "./pages/venues";
+// import Login from "./pages/login";
 import SignUp from "./pages/signup";
 import CalendarComponent from "./components/calender";
 import RSVPform from "./pages/RSVPform";
@@ -37,10 +37,10 @@ export default function RouterSetup() {
      <Route path="/" element={<Layout />}>
         <Route path="/CreateEvent" element={<CreateEvent />} />
         <Route path="/my-events" element={<MyEvents />} />
-        <Route path="/unlocks" element={<Unlocks />} />
+        {/* <Route path="/unlocks" element={<Unlocks />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/venues" element={<Venues />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} /> */}
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/calendar" element={<CalendarComponent />} />
         <Route path="/RSVPform" element={<RSVPform />} />
