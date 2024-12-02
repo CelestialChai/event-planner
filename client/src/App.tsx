3c496c221480837e5839d40e4681de4c6dbcd4cd
OKenne <175995429+O-KenneDevWorks@users.noreply.github.com>
2024-12-01 20:50:29 -0500

    converted from react routes to createBrowserRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,68 +1,23 @@
-import { Routes, Route, useLocation, Outlet } from 'react-router-dom';
-import NavigationBar from "./components/Nav";
-import "./App.css";
-import { Box, CssBaseline } from "@mui/material";
-import HomePage from './pages/home';
-import CreateEvent from "./pages/CreateEvent";
-import MyEvents from "./pages/myevents";
-import Unlocks from "./pages/unlocks";
-import AboutUs from "./pages/aboutus";
-import Venues from "./pages/venues";
-import WeddingLandingPage from "./pages/LandingPage"
-import Login from "./pages/login";
-import SignUp from "./pages/signup";
-import CalendarComponent from "./components/calender";
-import RSVPform from "./pages/RSVPform";
-import GoogleSignIn from './components/GoogleSignIn';
-import ErrorPage from './pages/error';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { CssBaseline, Box } from '@mui/material';
+import NavigationBar from './components/Nav';
 
-  // Debug Component to Log Current Location
-const DebugLocation: React.FC = () => {
-  const location = useLocation();
-  console.log('Current Path:', location.pathname);
-  return null; // This component doesn't render anything on the UI
-};
-
-// Layout Component for Navigation and Main Content
-function Layout() {
+const App: React.FC = () => {
   return (
     <>
       <CssBaseline />
-      <Box sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', minHeight: '100vh' }}>
+        {/* Navigation Bar */}
         <NavigationBar />
-        <Box sx={{ flexGrow: 1, p: 3 }}>
-          <main>
-            <DebugLocation /> {/* Logs the current path */}
-            <Outlet /> {/* Renders child routes */}
-          </main>
+
+        {/* Main Content */}
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Outlet />
         </Box>
       </Box>
     </>
   );
-}
+};
 
-  export default function App() {
-    return (
-      <Routes>
-       <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/create" element={<CreateEvent />} />
-          <Route path="/my-events" element={<MyEvents />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/unlocks" element={<Unlocks />} />
-          <Route path="/venues" element={<Venues />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/landfall" element={<WeddingLandingPage />} />
-          <Route path="/calendar" element={<CalendarComponent />} />
-          <Route path="/RSVPform" element={<RSVPform />} />
-          <Route path="google-sign-in" element={<GoogleSignIn />} />
-  
-          {/* Catch-All Route for Undefined Paths */}
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
-    );
-}
-
-
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,40 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import HomePage from './pages/home';
+import CreateEvent from './pages/CreateEvent';
+import MyEvents from './pages/myevents';
+import AboutUs from './pages/aboutus';
+import WeddingLandingPage from './pages/LandingPage';
+import Login from './pages/login';
+import SignUp from './pages/signup';
+import CalendarComponent from './components/calender';
+import ErrorPage from './pages/error';
+import GoogleSignIn from './components/GoogleSignIn';
 import './index.css';
-import App from '../src/App';
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'create', element: <CreateEvent /> },
+      { path: 'my-events', element: <MyEvents /> },
+      { path: 'about-us', element: <AboutUs /> },
+      { path: 'landfall', element: <WeddingLandingPage /> },
+      { path: 'login', element: <Login /> },
+      { path: 'sign-up', element: <SignUp /> },
+      { path: 'calendar', element: <CalendarComponent /> },
+      { path: 'google-sign-in', element: <GoogleSignIn /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
