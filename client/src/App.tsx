a8833ea8d3beee6d125397f0e51cb74085b14d2c
OKenne <175995429+O-KenneDevWorks@users.noreply.github.com>
2024-12-01 20:57:17 -0500

    Adjustments for createRouterBrowser

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 import { CssBaseline, Box } from '@mui/material';
+import { Outlet, useLocation } from 'react-router-dom';
 import NavigationBar from './components/Nav';
 
-const App: React.FC = () => {
+// Debug Component to Log Current Location
+const DebugLocation: React.FC = () => {
+  const location = useLocation();
+  console.log('Current Path:', location.pathname);
+  return null; // This component doesn't render anything on the UI
+};
+
+// Layout Component for Navigation and Main Content
+const Layout: React.FC = () => {
   return (
     <>
       <CssBaseline />
       <Box sx={{ display: 'flex', minHeight: '100vh' }}>
         {/* Navigation Bar */}
         <NavigationBar />
-
         {/* Main Content */}
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DebugLocation />
           <Outlet />
         </Box>
       </Box>
@@ -20,4 +28,4 @@ const App: React.FC = () => {
   );
 };
 
-export default App;
+export default Layout;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,40 +1,44 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import App from './App';
+import Layout from './App';
+import './index.css';
+
 import HomePage from './pages/home';
 import CreateEvent from './pages/CreateEvent';
 import MyEvents from './pages/myevents';
+import Unlocks from './pages/unlocks';
 import AboutUs from './pages/aboutus';
+import Venues from './pages/venues';
 import WeddingLandingPage from './pages/LandingPage';
 import Login from './pages/login';
 import SignUp from './pages/signup';
 import CalendarComponent from './components/calender';
-import ErrorPage from './pages/error';
+import RSVPform from './pages/RSVPform';
 import GoogleSignIn from './components/GoogleSignIn';
-import './index.css';
+import ErrorPage from './pages/error';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: 'create', element: <CreateEvent /> },
       { path: 'my-events', element: <MyEvents /> },
       { path: 'about-us', element: <AboutUs /> },
-      { path: 'landfall', element: <WeddingLandingPage /> },
+      { path: 'unlocks', element: <Unlocks /> },
+      { path: 'venues', element: <Venues /> },
       { path: 'login', element: <Login /> },
       { path: 'sign-up', element: <SignUp /> },
+      { path: 'landfall', element: <WeddingLandingPage /> },
       { path: 'calendar', element: <CalendarComponent /> },
+      { path: 'RSVPform', element: <RSVPform /> },
       { path: 'google-sign-in', element: <GoogleSignIn /> },
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
